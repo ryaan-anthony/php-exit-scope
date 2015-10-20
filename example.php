3a0983ff3af27756f0665c680a177484df27b15f
Tulino, Ryan <rtulino@ebay.com>
2015-10-19 22:47:54 -0400

    Update instructions

--- example.php
+++ example.php
@@ -2,7 +2,7 @@
 
 $filename = dirname(__FILE__).'/vendor/autoload.php';
 
-file_exists($filename) ? require $filename : exit('Run composer install'.PHP_EOL);
+file_exists($filename) ? require $filename : exit('Run `composer dump-autoload` to generate autoload files.'.PHP_EOL);
 
 $app = new App(function(Scope $scope){
 
